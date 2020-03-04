8a5cffa34184d6e77d12055016d1c9637ce8da72
Nico <nicolascarrizo6@gmail.com>
2020-03-04 19:58:22 -0300

    Matada las warnings

--- src/main/java/com/glamasw/petitamirestapi/entities/Dog.java
+++ src/main/java/com/glamasw/petitamirestapi/entities/Dog.java
@@ -1,7 +1,5 @@
 package com.glamasw.petitamirestapi.entities;
 
-import java.util.ArrayList;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/com/glamasw/petitamirestapi/services/DogService.java
+++ src/main/java/com/glamasw/petitamirestapi/services/DogService.java
@@ -3,7 +3,6 @@ package com.glamasw.petitamirestapi.services;
 import com.glamasw.petitamirestapi.repositories.DogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RestController;
 
 @Service
 public class DogService {
