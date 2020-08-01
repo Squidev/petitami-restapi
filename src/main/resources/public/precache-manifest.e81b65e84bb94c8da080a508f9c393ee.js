14356b1d7fd1a6360d6fad5514a883d30564e23c
Squidev <alesis.vq@gmail.com>
2020-08-01 15:46:43 -0300

    » Added missing bundle files.

---
+++ src/main/resources/public/precache-manifest.e81b65e84bb94c8da080a508f9c393ee.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f3ffc97838c0497fbdd8416fbb0d0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d559d36625fe7bc58112",
+    "url": "/static/css/2.b01f69de.chunk.css"
+  },
+  {
+    "revision": "4d0e406b85f002e49d5d",
+    "url": "/static/css/main.5f514416.chunk.css"
+  },
+  {
+    "revision": "d559d36625fe7bc58112",
+    "url": "/static/js/2.a9e11536.chunk.js"
+  },
+  {
+    "revision": "13d42ebd8d57253dbc01dec3881ffa3b",
+    "url": "/static/js/2.a9e11536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d0e406b85f002e49d5d",
+    "url": "/static/js/main.027e0874.chunk.js"
+  },
+  {
+    "revision": "8ae5ff30ce4af0370b6b",
+    "url": "/static/js/runtime-main.fa6fa02a.js"
+  },
+  {
+    "revision": "41a9ce8a76e6b78ab367ab389e95f3f4",
+    "url": "/static/media/email-logo.41a9ce8a.png"
+  },
+  {
+    "revision": "1d12cab8d56498c32608dc1a207d84d3",
+    "url": "/static/media/facebook-logo.1d12cab8.png"
+  },
+  {
+    "revision": "bb773893c3f90d8a759bf7813fa49daa",
+    "url": "/static/media/instagram-logo.bb773893.png"
+  },
+  {
+    "revision": "4f325f5595a05428bfd8422c9ab9ab4c",
+    "url": "/static/media/pet-avatar.4f325f55.png"
+  },
+  {
+    "revision": "aa92036ca0b83c32d5ea6f7b6a3e102b",
+    "url": "/static/media/twitter-logo.aa92036c.png"
+  }
+]);
